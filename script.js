125d3881afc8e6fddca0fb941a6e6a1af67b04e2
Ian Zolitor <ianzolitor@gmail.com>
2017-04-12 17:10:17 -0400

    exercise2

--- script.js
+++ script.js
@@ -6,18 +6,40 @@ var numberOne= 7;
 
 var numberTwo = 9;
 
-var total = numberOne + numberTwo
+var total = numberOne + numberTwo;
 
-console.log(total)
+console.log(total);
 
 var a = "String One"
 
 var b = "String Two"
 
-var c = a + b
-console.log(c)
+var c = a + b;
+console.log(c);
 
 
-var folk ["Gary Davis", "Uncle Dave Macon", "Mississippi John Hurt"]
-var rock ["Eric Clapton", "Jimi Hendrix", ]
-var genres ["folk", "rock"]
+// Exercise 2
+
+var folk =["Gary Davis", "Uncle Dave Macon", "Mississippi John Hurt"];
+
+var rock =["Eric Clapton", "Jimi Hendrix", "The Who"];
+
+var genres =[folk, rock];
+
+function music () {
+	for (var i=0; i<1;i++) {
+		subGenres(genres[i]);
+		
+	}
+
+}
+
+function subGenres (anyname) {
+	for (var i=0; i<2;i++ ){
+		console.log(anyname[i]);
+	}
+	
+	
+}
+
+music();
