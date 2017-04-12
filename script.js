0298bec3faf4244b03873e4de55795a6a86a243a
Ian Zolitor <ianzolitor@gmail.com>
2017-04-12 16:50:02 -0400

    exercise 1

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+// Exercise 1
+
+var numberOne= 7;
+
+var numberTwo = 9;
+
+var total = numberOne + numberTwo
+
+console.log(total)
+
+var a = "String One"
+
+var b = "String Two"
+
+var c = a + b
+console.log(c)
+
+
+var folk ["Gary Davis", "Uncle Dave Macon", "Mississippi John Hurt"]
+var rock ["Eric Clapton", "Jimi Hendrix", ]
+var genres ["folk", "rock"]
